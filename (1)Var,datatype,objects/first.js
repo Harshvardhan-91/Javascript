adc9e53254fa55a3073f64c05d07092b9f72ec02
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-08 08:10:10 +0530

    added

---
+++ (1)Var,datatype,objects/first.js
@@ -0,0 +1,20 @@
+console.log("Hello this is javascript");
+
+let a = 5;
+let b = 6;
+let c = "Harsh";
+
+console.log(a + b);
+console.log(typeof a, typeof b, typeof c);
+
+const a1 = 6; //now a1 can't be changed
+//a1 = a1 + 6 --> Not allowed
+
+//var vs let-----------------
+
+//most of the time we will use let instead of var
+// let is blocked scoped while var is globally scoped
+
+//Primitve data types are basic datatypes in javascript like NULL, Number, Undefined , String , Symbol , Boolean
+
+//Objects are non primitive datatypes
---
+++ (1)Var,datatype,objects/objects.js
@@ -0,0 +1,11 @@
+// An object can be created as-->
+
+let item = {
+  name: "red bulb",
+  price: "150",
+};
+
+// name and price are keys while red bulb and 150 are values
+
+item.name2 = "lamp"; //adding new key and value
+console.log(item);
