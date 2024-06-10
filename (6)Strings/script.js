6d7ad6e42d0cc2aaacf7a722bc48e8f0f7a2b2b0
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-10 12:04:50 +0530

    added

---
+++ (6)Strings/script.js
@@ -0,0 +1,10 @@
+console.log("This is strings");
+let a = "Harsh";
+console.log(a[0]);
+console.log(a.length);
+
+//Template literal
+
+let real_name = "Harsh";
+let friend = "Rohan";
+console.log(`His name is ${real_name} and his freinds name is ${friend}`);
