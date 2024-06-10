d42677e5c76802c09f40505b8a2b992a14c65ac4
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-10 12:56:22 +0530

    added

--- (6)Strings/script.js
+++ (6)Strings/script.js
@@ -16,3 +16,17 @@ console.log(b.toUpperCase());
 console.log(b.toLowerCase());
 console.log(b.length);
 console.log(b.slice(1, 4));
+console.log(b.slice(2));
+let b1 = "Harsh bhai";
+let b2 = b1.replace("bhai", "bhau");
+console.log(b2);
+
+//Concatenation
+let name1 = "Harsh";
+let name2 = "Agarwal";
+console.log(name1.concat(name2));
+
+//Trim(It removes white spaces)
+let x = " Harsh ";
+console.log(x);
+console.log(x.trim());
