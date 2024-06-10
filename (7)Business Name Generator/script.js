c21a591571904052f01d7caf0098074fb342f79a
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-10 13:02:59 +0530

    added

---
+++ (7)Business Name Generator/script.js
@@ -0,0 +1,18 @@
+/* Create a business name generator by combining list of adjectives and shop name and another word
+Adjective:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+Use of Arrays is not allowed
+*/
