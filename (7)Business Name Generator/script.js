36daefc0465000c97b1b38dcc9ae4db580f3b7ec
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-10 23:31:13 +0530

    added

--- (7)Business Name Generator/script.js
+++ (7)Business Name Generator/script.js
@@ -16,3 +16,31 @@ Hub
 
 Use of Arrays is not allowed
 */
+
+let rand = Math.random();
+let first, second, third;
+if (rand < 0.33) {
+  first = "Crazy";
+} else if (rand < 0.66 && rand >= 0.33) {
+  first = "Amazing";
+} else {
+  first = "Fire";
+}
+
+if (rand < 0.33) {
+  second = "Engine";
+} else if (rand < 0.66 && rand >= 0.33) {
+  second = "Foods";
+} else {
+  second = "Garments";
+}
+
+if (rand < 0.33) {
+  third = "Bros";
+} else if (rand < 0.66 && rand >= 0.33) {
+  third = "Limited";
+} else {
+  third = "Hub";
+}
+
+console.log(`${first} ${second} ${third}`);
