a2f097089c59fb976c46a838b7ce53b19cb0637b
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-10 11:29:28 +0530

    added

--- (3)Loops/script.js
+++ (3)Loops/script.js
@@ -7,3 +7,35 @@ let a = 1;
 }*/
 
 //for in loop--------------------------
+
+/*let obj = {
+  name: "Harry",
+  role: "Programmer",
+  company: "Code with Harry",
+};
+
+for (const key in obj) {
+  console.log(key);
+}*/
+
+//for of loop
+
+/*for (const c of "Harry") {
+  console.log(c);
+}*/
+
+//while loop
+
+/*let i = 5;
+while (i < 6) {
+  console.log(i);
+  i++;
+}*/
+
+//do while
+
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i < 6);
