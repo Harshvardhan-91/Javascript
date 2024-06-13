da8f69661acb73d8fc9892527800dfba39e761fa
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-13 12:01:51 +0530

    added

--- (9)Factorial/script.js
+++ (9)Factorial/script.js
@@ -1,1 +1,8 @@
 //Factorial using reduce and for loops
+let a = 6;
+function factorial(number) {
+  let arr = Array.from(Array(number + 1).keys()); //array method
+  let c = arr.slice(1).reduce((a, b) => a * b);
+  console.log(c);
+}
+factorial(a);
