21cf7f3ddfc86b686c97ef599d2f8d0f12cbb9e7
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-26 00:36:08 +0530

    added

--- (15)Dynamic website Builder/script.js
+++ (15)Dynamic website Builder/script.js
@@ -1,1 +1,12 @@
-function createCard(title, cName, views, monthsOld, duration) {}
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+  let html = ``;
+}
+
+createCard(
+  "Introduction to Backend | Sigma Web Dev video #2",
+  "CodeWithHarry",
+  560000,
+  7,
+  "31:22",
+  "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw"
+);
