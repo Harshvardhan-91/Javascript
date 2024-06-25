10a21d66296989b57689cad17cb54572fac62600
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-25 23:49:54 +0530

    added

---
+++ (18)Await and Fetch API/script.js
@@ -0,0 +1,30 @@
+/*async function getData() {
+  //simulate getting data from a server
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(455);
+    }, 3500);
+  });
+}*/
+
+async function getData() {
+  //simulate getting data from a server
+  let x = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  let data = await x.json();
+  return data;
+}
+
+async function main() {
+  console.log("Loading modules");
+  console.log("Do something else");
+  console.log("Load data");
+  let data = await getData();
+  console.log(data);
+  console.log("process data");
+  console.log("task 2");
+}
+main();
+// data.then((v) => {
+//   console.log(data);
+//   console.log("Process data");
+// });
