8168bd5c520493f7c78a2b73753bba6f9c443e3f
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-26 12:31:25 +0530

    added

--- (15)Dynamic website Builder/script.js
+++ (15)Dynamic website Builder/script.js
@@ -1,5 +1,30 @@
 function createCard(title, cName, views, monthsOld, duration, thumbnail) {
-  let html = ``;
+  let viewStr;
+  if (views < 1000000) {
+    viewStr = views / 1000 + "K";
+  } else if (views > 1000000) {
+    viewStr = views / 1000000 + "M";
+  } else {
+    viewStr = views / 1000 + "K";
+  }
+
+  let html = ` <div class="card">
+        <div class="img">
+          <img
+            src="${thumbnail}"
+            alt=""
+          />
+          <div class = "capsule">${duration}</div>
+          
+        </div>
+        <div class="text">
+          <h1>${title}</h1>
+          <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+        </div>
+      </div>`;
+
+  document.querySelector(".container").innerHTML =
+    document.querySelector(".container").innerHTML + html;
 }
 
 createCard(
